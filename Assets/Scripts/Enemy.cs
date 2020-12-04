817eba92ac9ae7154d2185d890aeea137b3c2615
Thana Sithisombath <sithisombath@cpp.edu>
2020-12-04 15:47:39 -0800

    simple health system.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float cooldown = 2.0f;
+    private bool isOnCooldown;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!isOnCooldown)
+        {
+            StartCoroutine(Damaging(collision.gameObject));
+        }
+    }
+
+    IEnumerator Damaging(GameObject player)
+    {
+        isOnCooldown = true;
+        player.GetComponent<Health>().TakeDamage();
+        yield return new WaitForSeconds(cooldown);
+        isOnCooldown = false;
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage()
+    {
+        currentHealth--;
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Is Dead.");
+        }
+    }
+}
