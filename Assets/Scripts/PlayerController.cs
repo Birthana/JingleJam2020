f6dc9ca811c1dc4c97854433f7fc949252e5ea05
Thana Sithisombath <sithisombath@cpp.edu>
2020-12-04 13:59:48 -0800

    simple platformer controllers

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    public bool isJumping = false;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(h * speed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
