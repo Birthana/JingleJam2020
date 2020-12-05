571821e3a5cc51d22bc7518cc02e9b3c1c164d56
Thana Sithisombath <sithisombath@cpp.edu>
2020-12-05 13:21:45 -0800

    player animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,26 +6,70 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2.0f;
     public bool isJumping = false;
 
     public GameObject playerFeet;
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
+        if (h == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+            if(h > 0)
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+            else
+            {
+                transform.rotation = new Quaternion(0, 180, 0, 0);
+            }
+        }
         rb.velocity = new Vector2(h * speed, rb.velocity.y);
+        CheckJump();
+        ApplyMultipliers();
+    }
+
+    private void CheckJump()
+    {
         if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
         {
             isJumping = true;
             rb.velocity = Vector2.up * jumpForce;
+            AudioManager.instance.PlaySound(3);
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+    }
+
+    private void ApplyMultipliers()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 }
