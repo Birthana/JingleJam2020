224ebd2cf9b3f2ba31237b53576fd1a96ea3746f
Thana Sithisombath <sithisombath@cpp.edu>
2020-12-04 20:50:55 -0800

    simple background shader.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+    public TextMeshProUGUI dialogueText;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DisplayDialogue("Test, Test, Test, Test, Test");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DisplayDialogue(string dialogue)
+    {
+        dialogueText.text = dialogue;
+    }
+}
