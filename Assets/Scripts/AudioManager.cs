eb8e72420196724ef6ba5a2b4def35bbb2434aa7
Thana Sithisombath <sithisombath@cpp.edu>
2020-12-05 11:19:59 -0800

    platformer/fetch quest

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public AudioClip[] audios;
+    public AudioSource backgroundPlayer;
+    public AudioSource soundPlayer;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayBackGround(int audioClip)
+    {
+        backgroundPlayer.clip = audios[audioClip];
+        backgroundPlayer.Play();
+    }
+
+    public void PlaySound(int audioClip)
+    {
+        soundPlayer.clip = audios[audioClip];
+        soundPlayer.Play();
+    }
+}
